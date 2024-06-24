8ecd8f063c51fe9dede152ea271f0d5b57c31b2f
zj kkk <npckei217@gmail.com>
2024-06-24 15:36:16 +0800

    feat:update

--- dist/utils/constants.d.ts
+++ dist/utils/constants.d.ts
@@ -2,7 +2,7 @@ import { BigNumber } from 'ethers';
 export declare const UNIVERSAL_ROUTER_ADDRESS: (chainId: number) => string;
 export declare const UNIVERSAL_ROUTER_CREATION_BLOCK: (chainId: number) => number;
 export declare const WETH_ADDRESS: (chainId: number) => string;
-export declare const PERMIT2_ADDRESS = "0x000000000022D473030F116dDEE9F6B43aC78BA3";
+export declare const PERMIT2_ADDRESS = "0xB952578f3520EE8Ea45b7914994dcf4702cEe578";
 export declare const CONTRACT_BALANCE: BigNumber;
 export declare const ETH_ADDRESS = "0x0000000000000000000000000000000000000000";
 export declare const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
